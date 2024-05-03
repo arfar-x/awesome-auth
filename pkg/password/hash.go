6ea98f8a276605d173193331eceab40137c701b6
Alireza Farhanian <aariow01@gmail.com>
2024-05-03 22:28:29 +0330

    add: password hash

--- pkg/password/hash.go
+++ pkg/password/hash.go
@@ -1,9 +1,18 @@
 package password
 
+import "golang.org/x/crypto/bcrypt"
+
 func Check(value1 string, value2 string) bool {
+	if err := bcrypt.CompareHashAndPassword([]byte(value1), []byte(value2)); err != nil {
+		return false
+	}
 	return true
 }
 
-func Make(value string) string {
-	return value
+func Make(value string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(value), 4)
+	if err != nil {
+		panic("Password hash generation failed.")
+	}
+	return string(hash), nil
 }
