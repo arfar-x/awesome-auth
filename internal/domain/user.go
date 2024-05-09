f12006ef535c90202e73497fcfd7370cfab03448
Alireza Farhanian <aariow01@gmail.com>
2024-05-09 15:33:56 +0330

    add: time field to user-domain

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,9 +1,13 @@
 package domain
 
+import "time"
+
 type UserDomain struct {
 	Username  string
 	Email     string
 	FirstName string
 	LastName  string
 	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
