ed6bcdbcc0e5fe275e8b0a6fcefdf35bb3fd4b2f
Alireza Farhanian <aariow01@gmail.com>
2024-05-09 15:32:00 +0330

    add: user-show response resource

--- internal/core/http/resources/auth.go
+++ internal/core/http/resources/auth.go
@@ -1,11 +1,25 @@
 package resources
 
-import "time"
+import (
+	"time"
+
+	"awesome-auth/internal/domain"
+)
 
 type UserShow struct {
-	FirstName string
-	LastName  string
-	Email     string
-	Username  string
-	CreatedAt time.Time
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func UserShowResource(model domain.UserDomain) UserShow {
+	return UserShow{
+		FirstName: model.FirstName,
+		LastName:  model.LastName,
+		Email:     model.Email,
+		Username:  model.Username,
+		CreatedAt: model.CreatedAt,
+	}
 }
