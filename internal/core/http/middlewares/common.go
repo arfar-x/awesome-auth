a0af1da2f2433e298cf29cbc9f3407544d2007d9
Alireza Farhanian <aariow01@gmail.com>
2024-05-24 15:00:49 +0330

    fix: mistaken middleware message

--- internal/core/http/middlewares/common.go
+++ internal/core/http/middlewares/common.go
@@ -15,7 +15,7 @@ func CommonMiddlewares(router *gin.RouterGroup) {
 func CheckTokenExists() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		if _, ok := ctx.Request.Header["Authorization"]; !ok {
-			ctx.AbortWithStatusJSON(response.Unauthorized("Unauthorized middleware.", nil))
+			ctx.AbortWithStatusJSON(response.Unauthorized("Unauthorized.", nil))
 			return
 		}
 
